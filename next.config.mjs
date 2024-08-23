0bfcdd8970770fce5f90185ae74c8bfeae1ddfac
salman <salmannisthar123@gmail.com>
2024-08-24 01:43:21 +0530

    homePage implementation finished

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+        remotePatterns:[
+            {
+                protocol:'https', 
+                hostname:'www.pexal.com' //add host name
+            }
+        ]
+    }
+};
 
 export default nextConfig;
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,30 @@
 import Image from "next/image";
+import style  from './homes.module.css'
 
 export default function Home() {
   return (
-    <main>
-      Home Page
+    <main className={style.container}>
+      
+      <div className={style.textContainer}>
+        <h1 className={style.title}>Creative Thoughts Agency</h1>
+        <p className={style.description}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.       
+        </p>
+
+        <div className={style.buttons}>
+          <button className={`${style.button}`}>Learn More</button>
+          <button className={`${style.button}`}>Contact</button>
+        </div>
+
+        <div className={`${style.brands}`}>
+
+          <Image src={"/brands.png"} alt="" fill className={style.brandImg} />
+        </div>
+
+      </div>
+      <div className={style.imgContainer}>
+      <Image src={"/hero.gif"} alt="" fill className={style.heroImg} />
+      </div>
     </main>
   );
 }
