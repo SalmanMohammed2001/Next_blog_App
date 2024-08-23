6e2f8c2a1a4f9a92a857a3adad3772fd2ba976c7
salman <salmannisthar123@gmail.com>
2024-08-24 00:31:10 +0530

    add respone navbar

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,8 +1,14 @@
+"use client"
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import NavLink from './navLink/navLink'
+import style from './links.module.css'
 
 const Links = () => {
+
+    const[open,setOpen]=useState(false);
+
+
 
     const link =[
         {
@@ -26,16 +32,50 @@ const Links = () => {
         }
     ]
 
-  return (
+     //Tampory
+     const session=true;
+     const isAdmin=true;
 
-    <div className={'flex items-center gap-[30px]'}>
+  return (
+<div className={`flex items-center gap-[10px] text-sm  ${style.container}`}>
+    <div className={`flex items-center gap-[10px] text-sm ${style.links}`}>
           {
        link.map((link)=>{
-        return <NavLink item={link} />
+        return <NavLink item={link} key={link.title}/>
+    })
+  }
+  {
+
+    session ? (
+    
+        <>
+    {isAdmin &&   <NavLink item={{title:"Admin",path:"/admin"}}/>}
+             <button className='p-[10px] cursor-pointer font-bold bg-white text-[#0d0c22]'>Logout</button>
+
+             </>
+     ) :
+    (
+    <NavLink item={{title:"Login",path:"/login"}}/>
+    )
+
+  }
+    </div>
+
+  <button className={style.menuMutton} onClick={()=>setOpen((pre)=>!pre)}>Menu</button>
+  {
+    open &&  <div className={`${style.mobileLinks}`}>
+        
+        {
+       link.map((link)=>{
+        return <NavLink item={link} key={link.title}/>
     })
   }
     </div>
-  )
-}
+  }
+
+
+    </div>
+  );
+};
 
 export default Links
