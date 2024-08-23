18e6e6c4ca83d5651f8548ef5bb0b96677f8f2ba
salman <salmannisthar123@gmail.com>
2024-08-24 02:22:12 +0530

    contactPage implementation finished

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import style from './contact.module.css'
+import Image from 'next/image'
 
 const Contactpage = () => {
   return (
-    <div>Contactpage</div>
+    <div className={style.container}>
+      <div className={style.imgContainer}>
+        <Image src="/contact.png"  alt=""   fill/>
+      </div>
+      <div className={style.formContainer}>
+        <form className={style.form}>
+          <input type='text' placeholder='Name ans surname'/>
+          <input type='text' placeholder='Email Address'/>
+          <input type='text' placeholder='Phone Number (optional)'/>
+          <textarea className={style.area} name='' id='' color='30' rows="10" placeholder='Messsage'></textarea>
+          <button>Send Me</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import style from './footer.module.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={style.container}>
+      <div className={style.logo}>
+        lamadev
+      </div>
+      <div className={style.text}>
+        Lama creative thourhts agency @ All rights reservrd
+      </div>
+
+    </div>
   )
 }
 
